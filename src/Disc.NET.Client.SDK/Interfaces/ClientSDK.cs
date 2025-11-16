8de32dd5808f9de011db535b8f3e0f6689071830
01Dri <diegomagalhaesdev@gmail.com>
2025-11-15 22:46:22 -0300

    doc

--- src/Disc.NET.Client.SDK/Interfaces/ClientSDK.cs
+++ src/Disc.NET.Client.SDK/Interfaces/ClientSDK.cs
@@ -5,6 +5,8 @@ namespace Disc.NET.Client.SDK.Interfaces;
 public interface IClient
 {
     Task SendMessageAsync(string channelId,ApiMessage message, CancellationToken cancellation = default);
+
+    // Using ApiMessage temporarily, will create a specific class for receiving messages later
     Task<ApiMessage?> GetMessageAsync(string channelId, string messageId, CancellationToken cancellation = default);
 
 }
