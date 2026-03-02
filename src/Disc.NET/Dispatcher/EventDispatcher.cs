1496b2f73eb7c699327a2b348bb0146fdff2b80c
dridev <diegomagalhaesdev@gmail.com>
2026-03-02 20:18:54 -0300

    event dispatcher

---
+++ src/Disc.NET/Dispatcher/EventDispatcher.cs
@@ -0,0 +1,41 @@
+﻿using Disc.NET.Handlers;
+using Disc.NET.Shared.Configurations;
+using Disc.NET.Shared.Enums;
+using System.Reflection;
+using System.Text.Json;
+
+namespace Disc.NET.Dispatcher
+{
+    internal class EventDispatcher
+    {
+        private readonly List<IHandler> _handlers;
+
+        private readonly AppConfiguration _appConfiguration;
+        public EventDispatcher(AppConfiguration appConfiguration)
+        {
+            _appConfiguration = appConfiguration;
+            _handlers = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(t => typeof(IHandler).IsAssignableFrom(t)
+                    && !t.IsInterface
+                    && !t.IsAbstract)
+                    .Select(t => (IHandler)Activator.CreateInstance(t, appConfiguration)!)
+                    .ToList();
+        }
+        public async Task DispatchAsync(EventHandlerPayload payload)
+        {
+            var eventType = payload.EventType;
+            var handlers = _handlers.Where(x => x.GetEventType() == eventType).ToList();
+            foreach (var handler in handlers)
+            {
+                await handler.HandleAsync(payload.Data, _appConfiguration);
+            }
+        }
+    }
+
+    internal class EventHandlerPayload(GatewayEvent eventType, JsonDocument data)
+    {
+        public GatewayEvent EventType { get; } = eventType;
+        public JsonDocument Data { get; } = data;
+
+    }
+}
