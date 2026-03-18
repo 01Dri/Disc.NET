9be792f51732b0c2f4f91551625484af99f203c7
dridev <diegomagalhaesdev@gmail.com>
2026-03-17 22:45:50 -0300

    callback repository and test command

---
+++ src/Disc.NET.Shared/ComponentsCallbacksRepository.cs
@@ -0,0 +1,30 @@
+﻿namespace Disc.NET.Commands
+{
+    public class ComponentsCallbacksRepository
+    {
+
+        private static ComponentsCallbacksRepository? _instance;
+
+        private readonly Dictionary<string, Func<bool>> _callbacks;
+        public static ComponentsCallbacksRepository Instance => _instance ??= new ComponentsCallbacksRepository();
+
+        public ComponentsCallbacksRepository()
+        {
+            _callbacks = new Dictionary<string, Func<bool>>();
+        }
+        public void SaveCallback(string callbackId, Func<bool> callback)
+        {
+            _callbacks.Add(callbackId, callback);
+        }
+
+        public Func<bool>? GetCallback(string callbackId)
+        {
+            if (_callbacks.TryGetValue(callbackId, out var callback))
+            {
+                return callback;
+            }
+            return null;
+        }
+    }
+}
+
---
+++ tests/GenericBot/TestCommand.cs
@@ -0,0 +1,39 @@
+﻿using Disc.NET.Client.SDK.Messages;
+using Disc.NET.Client.SDK.Messages.Components;
+using Disc.NET.Client.SDK.Messages.Components.Buttons;
+using Disc.NET.Commands;
+using Disc.NET.Commands.Attributes;
+using Disc.NET.Commands.Contexts;
+using Disc.NET.Shared.Enums;
+
+namespace GenericBot
+{
+    [SlashCommand("test", InteractionType.SubCommand, "test")]
+    public class TestCommand : ISlashCommand
+    {
+        public async Task RunAsync(InteractionContext context, SlashCommandParamsResult @params)
+        {
+
+            var button = new ActionRowButtonComponentBuilder()
+              .AddButton(new ButtonComponent(ButtonStyle.Success, "teste")
+              {
+                  Label = "Teste",
+                  Callback = Callback
+
+              });
+
+            var message = new ApiMessage
+            {
+                MessageComponents = [button]
+            };
+
+            await context.Response.SendMessageAsync(message, CancellationToken.None);
+        }
+
+        private bool Callback()
+        {
+            return true;
+        }
+
+    }
+}
